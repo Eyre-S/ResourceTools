27b29a89e816c775e94e98041024ebd8da2c265a
Eyre_S <Sukazyo@outlook.com>
2020-08-25 15:19:54 +0800

    build 2.1 | 补充忘记提交的ResourcesPackage类（（

--- src/main/java/cc/sukazyo/restools/ResourcesPackage.java
+++ src/main/java/cc/sukazyo/restools/ResourcesPackage.java
@@ -5,15 +5,25 @@ import java.nio.charset.StandardCharsets;
 import java.util.Enumeration;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ResourcesPackage {
 	
+	public static final String PACKID = "suk-restools";
+	public static final String VERSION = "0.1-dev";
+	public static final int BUILD = 2;
+	
 	private final ProjectType type;
+	
+	private final String resRoot;
 	
 	private File root;
 	private JarFile jar;
 	
 	public ResourcesPackage(String resRoot) throws IOException {
+		
+		resRoot = cutPath(resRoot);
 		
 		String protocol = ResourcesPackage.class.getResource("").getProtocol();
 		if ("jar".equals(protocol)){
@@ -25,41 +35,46 @@ public class ResourcesPackage {
 			);
 			type = ProjectType.JAR;
 		} else if("file".equals(protocol)){
-			root = new File(ResourcesPackage.class.getResource("/" + resRoot).getFile());
+			root = new File(ResourcesPackage.class.getResource("/" + resRoot).getFile()).getParentFile();
 			type = ProjectType.DIR;
 		} else {
 			throw new IOException("Project File is not on a dir or in a jar.");
 		}
 		
+		this.resRoot = resRoot;
+		
 	}
 	
-	public InputStream getResourceStream (String path) throws IOException {
-		
-		if (path.charAt(0) == '\\' || path.charAt(0) == '/') {
+	public ProjectType getType () {
+		return type;
+	}
+	
+	public JarFile getJar () {
+		return jar;
+	}
+	
+	public File getRoot () {
+		return root;
+	}
+	
+	public boolean isDirPack () {
+		return type == ProjectType.DIR;
+	}
+	
+	public boolean isJarPack () {
+		return type == ProjectType.JAR;
+	}
+	
+	public ResFile getResource (String path) throws IOException {
+		if (path.charAt(0) == '\\' || path.charAt(0) == '/')
 			path = path.substring(1);
-		}
-		
-		if (type == ProjectType.DIR) {
-			return new FileInputStream(new File(root.getPath() + "/" + path));
-		} else if (type == ProjectType.JAR) {
-			for (Enumeration<JarEntry> e = jar.entries(); e.hasMoreElements(); ) {
-				JarEntry entry = e.nextElement();
-				if (entry.getName().contains(path)) {
-					return jar.getInputStream(entry);
-				}
-			}
-		}
-		throw new IOException("File not found in project : " + path);
-		
+		return new ResFile(this, resRoot + path);
 	}
 	
-//	p	if (type == ProjectType.DIR) {
-//		System.out.println(new File(root.getPath() + "/" + path).getPath());
-//	} else if (type == ProjectType.JAR) {
-//		System.out.println("Hi");
-//	}
-//}ublic void test (String path) {
-	
+	public ResDir getResDir (String path) throws IOException {
+		path = cutPath(path);
+		return new ResDir(this, resRoot + path);
+	}
 	
 	@Override
 	public String toString() {
@@ -71,8 +86,37 @@ public class ResourcesPackage {
 		return null;
 	}
 	
-	private enum ProjectType {
+	enum ProjectType {
 		DIR, JAR
+	}
+	
+	public void test () {
+		if (isJarPack()) {
+			for (Enumeration<JarEntry> e = getJar().entries(); e.hasMoreElements(); ) {
+				JarEntry entry = e.nextElement();
+				System.out.println(entry.getName());
+			}
+		} else if (isDirPack()) {
+			forEachDir(root);
+		}
+	}
+	
+	private static void forEachDir (File dir) {
+		for (File sub : dir.listFiles()) {
+			if (sub.isFile()) {
+				System.out.println(sub.getPath());
+			} else if (sub.isDirectory()) {
+				forEachDir(sub);
+			}
+		}
+	}
+	
+	private String cutPath (String path) {
+		Matcher matcher = Pattern.compile("^[\\/\\\\]?(\\S+?)[\\/\\\\]?$").matcher(path);
+		if (matcher.find()) {
+			path = matcher.group(1) + "/";
+		}
+		return path;
 	}
 	
 }
